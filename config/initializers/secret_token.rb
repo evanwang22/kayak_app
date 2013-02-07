84d0f00fbf21bb498eb85fd7be7c56591aedd3ab
evanwang <saasbook@saasbook.(none)>
2013-02-07 01:54:42 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KayakApp::Application.config.secret_token = '4facc541862b9cf3c57be3d061343849b2da4bda6da2794ee5f13fca9e1a034d812173adeab353b82e49588c15c321b9b6148cf807b6cc2e6a82ec87905f2168'
